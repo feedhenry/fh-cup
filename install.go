acfc40e3132350ed904a79878ae3ead5e14e0cb2
Jason Madigan <jason@jasonmadigan.com>
2017-08-28 11:48:32 +0100

    Updating to use new rhmap-ansible directory structure

--- install.go
+++ install.go
@@ -10,11 +10,11 @@ import (
 func InstallRHMAP(conf Config) {
 	log.Println("Running rhmap-ansible installer...")
 
-	var cmd = sh.Command("sh", "-c", fmt.Sprintf("docker run -v %s/inventories:/opt/app-root/src/inventories"+
+	var cmd = sh.Command("sh", "-c", fmt.Sprintf("docker run -v %s/inventory-templates:/opt/app-root/src/inventory-templates"+
 		" -v %s/generated:/opt/rhmap/templates/core"+
 		" -v %s:/opt/rhmap/templates/mbaas"+
 		" -e PLAYBOOK_FILE=/opt/app-root/src/playbooks/poc.yml"+
-		" -e INVENTORY_FILE=/opt/app-root/src/inventories/engineering/cup/fh-cup-host"+
+		" -e INVENTORY_FILE=/opt/app-root/src/inventory-templates/fh-cup-example"+
 		" -e OPTS=\"-e core_templates_dir=/opt/rhmap/templates/core -e mbaas_templates_dir=/opt/rhmap/templates/mbaas"+
 		" -e mbaas_project_name=mbaas -e core_project_name=core -e strict_mode=false --tags deploy\""+
 		" %s",
