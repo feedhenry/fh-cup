3925c11074f16c20ead6de79aaae248533e8edcb
Jason Madigan <jason@jasonmadigan.com>
2017-09-27 15:38:46 +0100

    Removing volume mount for inventories

--- install.go
+++ install.go
@@ -10,7 +10,7 @@ import (
 func InstallRHMAP(conf Config) {
 	log.Println("Running rhmap-ansible installer...")
 
-	var cmd = sh.Command("sh", "-c", fmt.Sprintf("docker run -v %s/inventory-templates:/opt/app-root/src/inventory-templates"+
+	var cmd = sh.Command("sh", "-c", fmt.Sprintf("docker run "+
 		" -v %s/generated:/opt/rhmap/templates/core"+
 		" -v %s:/opt/rhmap/templates/mbaas"+
 		" -e PLAYBOOK_FILE=/opt/app-root/src/playbooks/poc.yml"+
@@ -18,7 +18,7 @@ func InstallRHMAP(conf Config) {
 		" -e OPTS=\"-e core_templates_dir=/opt/rhmap/templates/core -e mbaas_templates_dir=/opt/rhmap/templates/mbaas"+
 		" -e mbaas_project_name=mbaas -e core_project_name=core -e strict_mode=false --tags deploy\""+
 		" %s",
-		conf.RhmapAnsibleDir, conf.CoreOpenShiftTemplates, conf.MBaaSOpenShiftTemplates, conf.RhmapAnsibleImage))
+		conf.CoreOpenShiftTemplates, conf.MBaaSOpenShiftTemplates, conf.RhmapAnsibleImage))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
